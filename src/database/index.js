a877f3663a8782c2d224da39251fab1145e62117
Leonardo Assuncao <leonardoassuncao12235@gmail.com>
2020-02-24 21:48:25 -0300

    Add config database

--- src/database/index.js
+++ src/database/index.js
@@ -1,9 +1,7 @@
 const mongoose = require('mongoose');
 
-const MONGO_URL = process.env.MONGO_URL || 'mongodb+srv://godisfaithful:4ETESCev4q7L@cluster0-1dzcg.mongodb.net/churchesradar?retryWrites=true&w=majority';
-
 mongoose.connect(
-  MONGO_URL,
+  process.env.MONGO_URL,
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
