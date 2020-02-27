9afd4a687fa261d1133561824422bc8ee3e62e2d
Leonardo Assuncao <leonardoassuncao12235@gmail.com>
2020-02-26 22:10:16 -0300

    Melhorado a consulta no banco

--- src/controllers/searchController.js
+++ src/controllers/searchController.js
@@ -3,11 +3,12 @@ const Church = require('../models/Church');
 
 router.get('/', async (req, res) => {
   const { latitude, longitude, name } = req.query;
+  const regex = new RegExp(name.toLowerCase().trim(), 'i')
 
   const churches = await Church.find({
-    name: {
-      $in: name,
-    },
+    $or: [
+      { church_name: { $regex: regex } },
+      { name: { $regex: regex } }],
     location: {
       $near: {
         $geometry: {
